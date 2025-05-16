d088bf31cad231a288a5eda3f821e08be02006fa
Aizaz Ahmad <aizazahmed476@gmail.com>
2025-05-15 18:55:37 -0700

    Dark and Light mode added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useContext } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import './styles/components/App.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import ThemeContext from "./context/theme.context";
 
 function App() {
-  const dark = false;
+  const {dark} = useContext(ThemeContext)
   return (
     <div className={`App-${dark ? 'dark' : 'light'}`}>
      <Header/>
