f47b294c7b8691cd516a867c9c46263ba33da73f
Aizaz Ahmad <aizazahmed476@gmail.com>
2025-05-19 18:01:19 -0700

    Working on context API

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import WeatherContext from '../context/weather.context'
+
+
 
 function Place() {
+
+  const {place} = useContext(WeatherContext);
   return (
     <div className='Place'>
-        <i className='bi bi-geo-alt-fill'></i> <b>London</b>, United Kingdom
+        <i className='bi bi-geo-alt-fill'></i> <b>{place.name}</b>, {''} {place.country}
 
     </div>
   )
