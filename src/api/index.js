a57e77d331ab1a5f631b01bf8c8c9d71873a4cca
Aizaz Ahmad <aizazahmed476@gmail.com>
2025-05-19 19:00:52 -0700

    Making API Calls

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,32 @@
-import currentWeather from './current-weather.json';
-import dailyForecast from "./daily-forecast.json";
-import hourlyForecast from "./hourly-focus.json"
+import axios from 'axios';
 
-function getCurrentWeather() {
-    return currentWeather.data;
+
+export async function getWeatherData (
+    endpoint,
+    place_id,
+    measurementSystem
+) {
+
+
+const options = {
+  method: 'GET',
+  url: `https://ai-weather-by-meteosource.p.rapidapi.com/${endpoint}`,
+  params: {
+    place_id,
+    language: 'en',
+    units: measurementSystem,
+  },
+  headers: {
+    'x-rapidapi-key': '1de4a7878cmsh808568b3c0e4379p1da997jsnb556ead5afd9',
+    'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	return response.data;
+} catch (error) {
+	console.error(error);
 }
 
-function getDailyForecast() {
-    return dailyForecast.daily.data
 }
-
-function getHourlyForecast() {
-    return hourlyForecast.hourly.data;
-}
-
-export{getCurrentWeather, getDailyForecast,getHourlyForecast};
