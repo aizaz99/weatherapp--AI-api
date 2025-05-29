a946e9dd339f70ae9988723b5c8051419f18c2fb
Aizaz Ahmad <aizazahmed476@gmail.com>
2025-05-28 18:27:35 -0700

    Fully Functional Search Feature Added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,50 +1,59 @@
-import '../styles/components/Search.scss';
-import { searchPlaces } from '../api';
 import { useContext, useState } from 'react';
+import { searchPlaces } from '../api';
 import WeatherContext from '../context/weather.context';
+import '../styles/components/Search.scss';
 
 function Search() {
-  const {setPlace} = useContext(WeatherContext)
+  const { setPlace } = useContext(WeatherContext);
   const [text, setText] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [openSearchResults, setOpenSearchResults] = useState(false);
 
   async function onSearch(e) {
-    const input = e.target.value;
-    setText(input);
-    if (input.trim()) {
-      const data = await searchPlaces(input);
-      setSearchResults(data || []);
-    } else {
-      setSearchResults([]);
-    }
+    setText(e.target.value);
+    const data = await searchPlaces(e.target.value);
+    setSearchResults(data);
+    setOpenSearchResults(data.length);
   }
-const changePlace = () => {
 
-}
+  const changePlace = (place) => {
+    setPlace(place);
+    setText('');
+    setOpenSearchResults(false);
+  };
+
   return (
-    <div className="search-container">
-      <div className="search-icon">
-        <i className='bi bi-search'></i>
-      </div>
-      <div className="search-input">
-        <input
-          type='text'
-          name='search-city'
-          placeholder="Search city"
-          value={text}
-          onChange={onSearch}
-        />
-      </div>
-      <div className='search-results'>
-        <div className='results-container'>
-          {searchResults.map((place) => (
-            <div className='result' key={place.place_id} onClick={changePlace}>
-              {place.name}, {place.adm_area1}, {place.country}
-            </div>
-          ))}
+    <>
+      <div className='search-container'>
+        <div className='search-icon'>
+          <i className='bi bi-search'></i>
         </div>
+        <div className='search-input'>
+          <input
+            type='text'
+            name='search-city'
+            placeholder='Search city ...'
+            value={text}
+            onChange={onSearch}
+          />
+        </div>
+        {openSearchResults && (
+          <div className='search-results'>
+            <div className='results-container'>
+              {searchResults.map((place) => (
+                <div
+                  className='result'
+                  key={place.place_id}
+                  onClick={() => changePlace(place)}
+                >
+                  {place.name}, {place.adm_area1}, {place.country}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 }
 
